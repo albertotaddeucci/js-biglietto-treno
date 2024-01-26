b32ca1e8cfb3e25f8becb27055e6cebfc1662a4f
alberto <alberto.taddeucci@gmail.com>
2024-01-26 17:41:48 +0100

    es.completato + bonus

--- js/script.js
+++ js/script.js
@@ -1,38 +1,49 @@
-
-const userYear = prompt("Inserisci anno di nascita (formato: YYYY)");
-const distance = prompt("Quanti chilometri devi percorrere?");
 const date = new Date();
-const userAge = date.getFullYear() - userYear;
+const userYear = prompt("Inserisci anno di nascita (formato: YYYY)");
+const currentYear = date.getFullYear()
 
-// costo biglietto intero
-const ticketPrice = 0.21 * distance;
-let discountPrice;
+if(userYear<1900 || isNaN(userYear) || userYear>currentYear) {
+    alert("Attenzione: data inserita non valida");
+   
+}
 
-// controllo se dati inseriti sono numerici
-if ( isNaN(userYear) || isNaN(distance)) {
-    alert("Attenzione: non è stato inserito un valore numerico in uno dei campi precedenti");
+const distance = prompt("Quanti chilometri devi percorrere?");
 
-} else {
-
-    // calcolo sconto    
-    if (userAge<18){
-        discountPrice = ticketPrice * 0.2;
-          
-    } else if (userAge>65){
-        discountPrice = ticketPrice * 0.4;
-      
-        
-    } else {
-        discountPrice = 0;
-    }
+if (isNaN(distance) || distance<1){
+    alert("Attenzione: distanza inserita non valida");
 
 }
 
+const userAge = currentYear - userYear;
+
+
+// costo biglietto intero
+const ticketPrice = 0.21 * distance;
+
+// calcolo sconto    
+let discountPrice;
+
+if (userAge<18){
+    discountPrice = ticketPrice * 0.2;
+      
+} else if (userAge>65){
+    discountPrice = ticketPrice * 0.4;
+      
+} else {
+    discountPrice = 0;
+}
 
 // biglietto scontato
 const finalPrice = ticketPrice - discountPrice;
 
-// visualizzazione a schermo arrotondata a 2 decimali
-document.getElementById("ticketprice").innerHTML = 
+
+if (isNaN(distance) || distance<1 || userYear<1900 || isNaN(userYear) || userYear>currentYear){
+    document.getElementById("ticketprice").innerHTML = "Errore"
+
+} else {
+    document.getElementById("ticketprice").innerHTML = 
     `<b>${finalPrice.toFixed(2)}€</b>`
 ;
+
+}
+
