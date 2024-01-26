a7b8a2bdddda5c14c342c21d3fc9deeb86cade39
alberto <alberto.taddeucci@gmail.com>
2024-01-26 16:25:17 +0100

    bonus - controllo dati

--- js/script.js
+++ js/script.js
@@ -1,35 +1,33 @@
-/*
-Il programma dovrà chiedere all'utente il numero di chilometri 
-che vuole percorrere e l'età del passeggero.
 
-Calcolare il prezzo totale del viaggio, secondo queste regole:
-- il prezzo del biglietto è definito in base ai km (0.21 € al km)
-- va applicato uno sconto del 20% per i minorenni
-- va applicato uno sconto del 40% per gli over 65.
-
-L'output del prezzo finale va messo fuori in forma umana 
-(con massimo due decimali, per indicare centesimi sul prezzo). 
-*/
-
-const userYear = prompt("Inserisci anno di nascita");
+const userYear = prompt("Inserisci anno di nascita (formato: YYYY)");
 const distance = prompt("Quanti chilometri devi percorrere?");
 const date = new Date();
-const userAge = date.getFullYear() - userYear
+const userAge = date.getFullYear() - userYear;
 
 // costo biglietto intero
 const ticketPrice = 0.21 * distance;
-
-// calcolo sconto
 let discountPrice;
-if (userAge<18){
-    discountPrice = ticketPrice * 0.2;
 
-} else if (userAge>65){
-    discountPrice = ticketPrice * 0.4;
-    
+// controllo se dati inseriti sono numerici
+if ( isNaN(userYear) || isNaN(distance)) {
+    alert("Attenzione: non è stato inserito un valore numerico in uno dei campi precedenti");
+
 } else {
-    discountPrice = 0;
+
+    // calcolo sconto    
+    if (userAge<18){
+        discountPrice = ticketPrice * 0.2;
+          
+    } else if (userAge>65){
+        discountPrice = ticketPrice * 0.4;
+      
+        
+    } else {
+        discountPrice = 0;
+    }
+
 }
+
 
 // biglietto scontato
 const finalPrice = ticketPrice - discountPrice;
