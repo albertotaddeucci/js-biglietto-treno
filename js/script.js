aab8de79b9e38e7727b880186e6774b014b457ea
alberto <alberto.taddeucci@gmail.com>
2024-01-26 18:23:24 +0100

    fix: Ottimizzazione

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
 const date = new Date();
-const userYear = prompt("Inserisci anno di nascita (formato: YYYY)");
+let userYear = prompt("Inserisci anno di nascita (formato: YYYY)");
+
 const currentYear = date.getFullYear()
 
+// controllo anno
 if(userYear<1900 || isNaN(userYear) || userYear>currentYear) {
     alert("Attenzione: data inserita non valida");
-   
+    userYear = "errore"   
 }
 
-const distance = prompt("Quanti chilometri devi percorrere?");
+let distance = prompt("Quanti chilometri devi percorrere?");
+
+// controllo distanza
 
 if (isNaN(distance) || distance<1){
     alert("Attenzione: distanza inserita non valida");
-
+    distance = "errore"
 }
 
+// calcolo età
 const userAge = currentYear - userYear;
-
 
 // costo biglietto intero
 const ticketPrice = 0.21 * distance;
@@ -37,13 +41,13 @@ if (userAge<18){
 const finalPrice = ticketPrice - discountPrice;
 
 
-if (isNaN(distance) || distance<1 || userYear<1900 || isNaN(userYear) || userYear>currentYear){
+// messaggio errore o visualizzazione a schermo prezzo
+if (userYear === "errore" || distance === "errore"){
     document.getElementById("ticketprice").innerHTML = "Errore"
-
 } else {
     document.getElementById("ticketprice").innerHTML = 
     `<b>${finalPrice.toFixed(2)}€</b>`
-;
-
 }
+
+
 
