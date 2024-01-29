68f439fc6b990c0c43f9ad2661d92eb742baa87a
alberto <alberto.taddeucci@gmail.com>
2024-01-29 12:07:25 +0100

    snack#1

---
+++ snack #1/script.js
@@ -0,0 +1,19 @@
+/*Crea un programma che chiede all'utente una temperatura in gradi Celsius
+ e la converte in Fahrenheit
+
+Visualizza la temperatura convertita.*/
+
+
+const tempCelsius = prompt("Inserisci la temperatura in gradi Celsius da convertire")
+
+if (isNaN(tempCelsius)){
+    alert ("Attenzione: inserire valore numerico")
+}
+
+let tempFaren;
+
+tempFaren = ( tempCelsius * 9/5 ) + 32
+
+// console.log(tempFaren)
+
+document.getElementById("conversion").innerHTML = `${tempFaren}°F`
